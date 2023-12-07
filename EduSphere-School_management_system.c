b9d1b870a0df492b1a42c0f6d32bf3e6fbc57671
Akas_Datta <012akasdatta@gmail.com>
2023-12-07 20:31:52 +0600

    searching book details and enroll student for class

--- EduSphere-School_management_system.c
+++ EduSphere-School_management_system.c
@@ -4,11 +4,24 @@
 // Structure to represent a class
 struct Class {
     char name[50];
+    char teacher[50];
+    char genre[50];
     char details[200];
+    int enrolled;  // Flag to indicate whether a student is enrolled
 };
 
-// Function prototype
+// Function prototypes
 void welcomePage();
+void studentMenu(struct Class classes[], int numClasses);
+void searchAndEnroll(struct Class classes[], int numClasses);
+
+// Function to display the welcome page
+void welcomePage() {
+    printf("Welcome to the School Management System!\n");
+    printf("Please identify yourself:\n");
+    printf("1. Teacher\n");
+    printf("2. Student\n");
+}
 
 // Function to display the student menu
 void studentMenu(struct Class classes[], int numClasses) {
@@ -16,7 +29,7 @@ void studentMenu(struct Class classes[], int numClasses) {
     
     do {
         printf("\nStudent Menu:\n");
-        printf("1. Search for a class\n");
+        printf("1. Search for a class and enroll\n");
         printf("2. List all classes\n");
         printf("3. View class name and details\n");
         printf("4. Exit\n");
@@ -26,9 +39,8 @@ void studentMenu(struct Class classes[], int numClasses) {
 
         switch (option) {
             case 1:
-                // Implement search for a class functionality
-                // You can prompt the user to enter a class name or other criteria for searching
-                printf("Searching for a class...\n");
+                // Implement search and enroll functionality
+                searchAndEnroll(classes, numClasses);
                 break;
 
             case 2:
@@ -65,20 +77,58 @@ void studentMenu(struct Class classes[], int numClasses) {
     } while (option != 4);
 }
 
-// Function to display the welcome page
-void welcomePage() {
-    printf("Welcome to the School Management System!\n");
-    printf("Please identify yourself:\n");
-    printf("1. Teacher\n");
-    printf("2. Student\n");
+// Function to search for a class and enroll
+void searchAndEnroll(struct Class classes[], int numClasses) {
+    char searchCriteria[50];
+    printf("Enter the class name, teacher name, or genre to search: ");
+    scanf("%s", searchCriteria);
+
+    int matchFound = 0;
+
+    for (int i = 0; i < numClasses; i++) {
+        if (strstr(classes[i].name, searchCriteria) != NULL ||
+            strstr(classes[i].teacher, searchCriteria) != NULL ||
+            strstr(classes[i].genre, searchCriteria) != NULL) {
+
+            // Display the search result
+            printf("Match found!\n");
+            printf("Class Name: %s\n", classes[i].name);
+            printf("Teacher: %s\n", classes[i].teacher);
+            printf("Genre: %s\n", classes[i].genre);
+            printf("Details: %s\n", classes[i].details);
+
+            // Check if the student is already enrolled
+            if (classes[i].enrolled) {
+                printf("You are already enrolled in this class.\n");
+            } else {
+                // Ask if the student wants to enroll
+                char enrollChoice;
+                printf("Do you want to enroll in this class? (y/n): ");
+                scanf(" %c", &enrollChoice);
+
+                if (enrollChoice == 'y' || enrollChoice == 'Y') {
+                    printf("Enrolled in the class!\n");
+                    classes[i].enrolled = 1;  // Set the enrolled flag
+                } else {
+                    printf("Not enrolled in the class.\n");
+                }
+            }
+
+            matchFound = 1;
+        }
+    }
+
+    if (!matchFound) {
+        printf("No matching classes found.\n");
+    }
 }
 
 int main() {
     // Dummy data for classes
     struct Class classes[3] = {
-        {"Math", "Introduction to Mathematics"},
-        {"Science", "Basic Science Concepts"},
-        {"History", "World History Overview"}
+        {"Math", "Mr. Smith", "Mathematics", "Introduction to Mathematics", 0},
+        {"Science", "Ms. Johnson", "Science", "Basic Science Concepts", 0},
+        {"History", "Mr. Davis", "History", "World History Overview", 0}
     };
 
     // Variables to store user choice and name
